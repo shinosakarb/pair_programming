23a80c39f9a47fe35efda5a7057168ce24fe813e
honeniq <honeniq@gmail.com>
2015-09-26 14:07:16 +0900

    Implement Step1

--- 20150926/muraniq/dqx/lib/dqx/base.rb
+++ 20150926/muraniq/dqx/lib/dqx/base.rb
@@ -1,4 +1,21 @@
 module Dqx
   class Base
+    def self.run
+      puts "スライムが現れた！"
+      
+      while(true)
+        puts "1. たたかう"
+        puts "2. にげる"
+        puts "コマンド？"
+
+        case gets.chomp
+        when "1"
+          puts "スライムを殴った"
+        when "2"
+          puts "逃げ出した！"
+          break
+        end
+      end
+    end
   end
 end
