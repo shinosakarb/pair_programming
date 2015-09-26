f1db2a0c9040de13e6f41219f7dd8ee1031d4bba
honeniq <honeniq@gmail.com>
2015-09-26 16:13:15 +0900

    Implement step2,3

--- 20150926/muraniq/dqx/lib/dqx/base.rb
+++ 20150926/muraniq/dqx/lib/dqx/base.rb
@@ -1,20 +1,45 @@
 module Dqx
   class Base
     def self.run
-      puts "スライムが現れた！"
+      hero = Hero.new(:level => 1, :attack_power => 4, :defence_power => 4, \
+                      :hit_point => 15, :magic_power => 0)
+      while(true)
+        case self.combat(hero)
+        when nil
+          break
+        end
+      end
+    end
+
+    def self.combat(hero)
+      slime = Slime.new(:name => "スライム", :attack_power => 5, :defence_power => 3, \
+                        :hit_point => 3, :experience_point => 1)
+      
+      puts slime.name + ("が現れた！")
       
       while(true)
-        puts "1. たたかう"
-        puts "2. にげる"
-        puts "コマンド？"
+        unless hero.alive?
+          puts "勇者は死んでしまった"
+          break
+        end
+
+        puts "勇者のHP" + hero.hit_point.to_s
+        puts "▼ コマンド？ [ 1. たたかう 2. にげる ]"
 
         case gets.chomp
         when "1"
-          puts "スライムを殴った"
+          hero.attack(slime)
         when "2"
           puts "逃げ出した！"
           break
         end
+
+        if slime.hit_point <= 0
+          puts slime.name + ("をやっつけた！")
+          break
+        end
+
+        slime.attack(hero)
       end
     end
   end
