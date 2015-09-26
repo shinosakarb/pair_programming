683dff56997536314ee4210013a7c935c4f63061
ogom <ogom@hotmail.co.jp>
2015-09-26 14:35:49 +0900

    Add dragoon start

--- 20150926/dragoon/lib/dragoon.rb
+++ 20150926/dragoon/lib/dragoon.rb
@@ -4,9 +4,16 @@ module Dragoon
   # Your code goes here...
 
   class Main
-    def self.run
-      puts "王様に呼ばれた勇者が、ドラゴンを倒した。"
-      puts "- 完 -"
+    class << self
+      def start
+        puts "王様に呼ばれた勇者が、ドラゴンを倒した。"
+
+        print "勇者の名前 > "
+        stdin = STDIN.gets.chomp.to_s
+        puts "勇者 #{stdin} !"
+
+        puts "- 完 -"
+      end
     end
   end
 end
